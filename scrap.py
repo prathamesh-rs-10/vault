fd2a648ec69495dd1621039c0b41c874c5281c61
Prathamesh Sawant <94855461+prathamesh-rs-10@users.noreply.github.com>
2024-08-16 17:42:07 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,28 +1,50 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
- 
+import psycopg2
+from sqlalchemy import create_engine
+from kafka import KafkaProducer
+
+# Web scraping
 url = 'https://screener.in/company/RELIANCE/consolidated/'
 webpage = requests.get(url)
-soup = bs(webpage.text,'html.parser')
- 
+soup = bs(webpage.text, 'html.parser')
 data = soup.find('section', id="profit-loss")
-tdata= data.find("table")
- 
-table_data = []
- 
-for row in tdata.find_all('tr'):
-    row_data = []
-    for cell in row.find_all(['th','td']):
-        row_data.append(cell.text.strip())
-    table_data.append(row_data)
- 
- 
-df_table = pd.DataFrame(table_data)
-df_table.iloc[0,0] = 'Section'
-df_table.columns = df_table.iloc[0]
-df_table = df_table.iloc[1:,:-2]
-for i in df_table.iloc[:,1:].columns:
-    df_table[i] = df_table[i].str.replace(',','').str.replace('%','/100').apply(eval)
-# df_table = df_table.set_index('')
-print(df_table)
+
+if data is not None:
+    tdata = data.find("table")
+    if tdata is not None:
+        table_data = []
+        for row in tdata.find_all('tr'):
+            row_data = []
+            for cell in row.find_all(['th', 'td']):
+                row_data.append(cell.text.strip())
+            table_data.append(row_data)
+
+        df_table = pd.DataFrame(table_data)
+        df_table.iloc[0, 0] = 'Section'
+        df_table.columns = df_table.iloc[0]
+        df_table = df_table.iloc[1:, :-2]
+
+        for i in df_table.iloc[:, 1:].columns:
+            df_table[i] = df_table[i].str.replace(',', '').str.replace('%', '/100').apply(eval)
+
+        print(df_table)
+
+        # Load data to Postgres
+        db_host = "192.168.3.66"
+        db_name = "postgres"
+        db_user = "ps"
+        db_password = "ps"
+        db_port = "5432"
+        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+        df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
+        print("Data loaded to Postgres")
+
+        # Produce data to Kafka
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+        for index, row in df_table.iterrows():
+            data = row.to_dict()
+            producer.send('profit_loss_data', value=data)
+        producer.close()
+        print("Data produced to Kafka")
