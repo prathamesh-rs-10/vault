4c40b3a29b168486d20f552190a4dc0d62f7d8b1
Prathamesh Sawant <94855461+prathamesh-rs-10@users.noreply.github.com>
2024-08-20 15:05:40 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
-from kafka import KafkaProducer
 
 # Web scraping
 url = 'https://screener.in/company/RELIANCE/consolidated/'
@@ -29,20 +28,18 @@ if data is not None:
         for i in df_table.iloc[:, 1:].columns:
             df_table[i] = df_table[i].str.replace(',', '').str.replace('%', '/100').apply(eval)
 
-    # ... (rest of your code remains the same)
+        # Transpose the dataframe
+        df_table = df_table.set_index('Section').transpose()
 
-# Transpose the dataframe
-df_table = df_table.set_index('Section').transpose()
+        # Add an incremental ID column
+        df_table['id'] = range(1, len(df_table) + 1)
 
-# Add an incremental ID column
-df_table['id'] = range(1, len(df_table) + 1)
-
-# Load data to Postgres
-db_host = "192.168.3.66"
-db_name = "postgres"
-db_user = "ps"
-db_password = "ps"
-db_port = "5432"
-engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
-df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
-print("Data loaded to Postgres")
+        # Load data to Postgres
+        db_host = "192.168.3.66"
+        db_name = "postgres"
+        db_user = "ps"
+        db_password = "ps"
+        db_port = "5432"
+        engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+        df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
+        print("Data loaded to Postgres")
